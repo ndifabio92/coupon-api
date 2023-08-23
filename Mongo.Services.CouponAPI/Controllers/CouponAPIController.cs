0bc0299d13d3878d356d1c927909d81aabf15408
Nicolás Di Fabio <ndifabio@icebergsolutions.com>
2023-08-23 00:08:01 -0300

    initial commit

---
+++ Mongo.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,139 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mongo.Services.CouponAPI.Data;
+using Mongo.Services.CouponAPI.Models;
+using Mongo.Services.CouponAPI.Models.Dto;
+using static Azure.Core.HttpHeader;
+
+namespace Mongo.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+
+        public CouponAPIController(AppDbContext db, IMapper mapper) 
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(coupon => coupon.CouponId.Equals(id));
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(coupon => coupon.CouponCode.ToLower() == code.ToLower());
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon coupon = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(coupon);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon coupon = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(coupon);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(coupon => coupon.CouponId == id);
+                _db.Coupons.Remove(coupon);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSucces = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ Mongo.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mongo.Services.CouponAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public object? Result {  get; set; }
+        public bool IsSucces { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
